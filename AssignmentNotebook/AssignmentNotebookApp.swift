f5f943ad4202e91fa45edbecbcaba79c85554ee4
Tessa Murray <tmurray@bsd220.org>
2025-01-29 08:50:15 -0800

    Initial Commit

---
+++ AssignmentNotebook/AssignmentNotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentNotebookApp.swift
+//  AssignmentNotebook
+//
+//  Created by Tessa Murray on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
