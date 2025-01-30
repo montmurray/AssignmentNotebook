478797722df41a2029cb1a0b6ed9c98feb70de35
Tessa Murray <tmurray@bsd220.org>
2025-01-30 07:51:19 -0800

    Created assignment list class

---
+++ AssignmentNotebook/AssignmentList.swift
@@ -0,0 +1,11 @@
+//
+//  AssignmentList.swift
+//  AssignmentNotebook
+//
+//  Created by Tessa Murray on 1/30/25.
+//
+import SwiftUI
+
+class AssignmentList: ObservableObject {
+    @Published var items = [AssignmentItem(course: "English", description: "Shakespeare Narrative", dueDate: Date()), AssignmentItem(course: "Physcology", description: "Two-Factor Theory Discussion", dueDate: Date()), AssignmentItem(course: "Calculus", description: "Related Rates Quiz", dueDate: Date()), AssignmentItem(course: "History", description: "Ottoman Empire Essay", dueDate: Date()), AssignmentItem(course: "Chemistry", description: "Periodic Table", dueDate: Date()), AssignmentItem(course: "Computer Science", description: "Assignment Notebook", dueDate: Date())]
+}
--- AssignmentNotebook/ContentView.swift
+++ AssignmentNotebook/ContentView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var assignmentItems = [AssignmentItem(course: "English", description: "Shakespeare Narrative", dueDate: Date()), AssignmentItem(course: "Physcology", description: "Two-Factor Theory Discussion", dueDate: Date()), AssignmentItem(course: "Calculus", description: "Related Rates Quiz", dueDate: Date()), AssignmentItem(course: "History", description: "Ottoman Empire Essay", dueDate: Date()), AssignmentItem(course: "Chemistry", description: "Periodic Table", dueDate: Date()), AssignmentItem(course: "Computer Science", description: "Assignment Notebook", dueDate: Date())]
+    @ObservedObject var assignmentList = AssignmentList()
     var body: some View {
         NavigationView{
             List {
-                ForEach(assignmentItems) { item in
+                ForEach(assignmentList.items) { item in
                     HStack {
                     VStack(alignment: .leading, content: {
                         Text(item.course).font(.headline)
@@ -22,8 +22,8 @@ struct ContentView: View {
                         Text(item.dueDate, style: .date)
                 }
                 }
-                .onMove(perform: { indices, newOffset in assignmentItems.move(fromOffsets: indices, toOffset: newOffset)})
-                .onDelete(perform: { indexSet in assignmentItems.remove(atOffsets: indexSet)})
+                .onMove(perform: { indices, newOffset in assignmentList.items.move(fromOffsets: indices, toOffset: newOffset)})
+                .onDelete(perform: { indexSet in assignmentList.items.remove(atOffsets: indexSet)})
             }
             .navigationBarTitle("Assignment Notebook", displayMode: .inline)
             .navigationBarItems(leading: EditButton())
