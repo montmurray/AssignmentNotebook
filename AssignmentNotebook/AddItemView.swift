d6a2ccf8bf1cf27e9dbd17aa7ca144dc22059215
Tessa Murray <tmurray@bsd220.org>
2025-01-30 08:01:41 -0800

    emergency commit, got stuck here again.

--- AssignmentNotebook/AddItemView.swift
+++ AssignmentNotebook/AddItemView.swift
@@ -20,7 +20,17 @@ struct AddItemView: View {
                     ForEach(Self.courses, id: \.self) { course in Text(course)
                     }
                 }
+                TextField("Description", text: $description)
+                DatePicker("Due Date", selection:  $dueDate, displayedComponents: .date)
             }
+            .navigationBarTitle("Add New Assignment", displayMode: .inline)
+            .navigationBarItems(trailing: Button("Save") {
+                if course.count > 0 && description.count > 0 {
+                    let item = AssignmentItem(id: UUID(), course: course, description: description, dueDate: dueDate)
+                    assignmentList.items.append(item)
+                    presentationMode.wrappedValue.dismiss()
+                }
+            })
         }
     }
 }
